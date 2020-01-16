922e207fb6fdcccffcb050345cf901f3486ea110
WonHoChoi <dnjsgh0721@naver.com>
2020-01-16 19:36:51 +0900

    remote repo 연결 후 첫 commit and push

--- 02_HelloGit/src/com/kh/git/HelloGitTest.java
+++ 02_HelloGit/src/com/kh/git/HelloGitTest.java
@@ -5,6 +5,7 @@ public class HelloGitTest {
 	public static void main(String[] args) {
 		System.out.println("안녕하세요!");
 		System.out.println("저녁맛있게 드셨나요?");
+		System.out.println("축하드립니다.");
 
 	}
 
